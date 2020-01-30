17574889d580fa20f5863b841a9074672098e5a5
victor7246 <ayan.sengupta007@gmail.com>
2020-01-30 15:10:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', encoding='utf-8') as f:
 if __name__ == "__main__":
     setup(
         name="jointtsmodel",
-        version="1.1",
+        version="1.2",
         description="jointtsmodel - library of joint topic-sentiment models",
         long_description=long_description,
         long_description_content_type='text/markdown',
